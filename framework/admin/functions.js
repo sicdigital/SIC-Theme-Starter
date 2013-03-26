7c659d8b9319ebeeeb6404cc7f062ce1078febd8
Michael Chacon <mike@sicdigital.com>
2013-03-25 22:55:01 -0400

    Removed framework to re-add as submodule

--- framework/admin/functions.js
+++
@@ -1,9 +0,0 @@
-(function($){
-$(function() {
-
-    $('#page_template').change(function() {
-        $('#page_slider_settings').toggle($(this).val() == 'default');
-    }).change();
-
-});
-})(jQuery);
