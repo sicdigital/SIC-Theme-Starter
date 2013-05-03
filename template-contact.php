8c010bd70655ecca16abcf7c99d1173d97d02035
Michael Chacon <mike@sicdigital.com>
2013-05-03 10:35:21 -0400

    Added info to about us staff section

--- template-contact.php
+++ template-contact.php
@@ -9,11 +9,12 @@ get_template_part('header');?>
 
 						
 				<?php while ( have_posts() ) : the_post(); ?>	
-						<h2>Please B In Touch</h2>
+				
+					<?php the_content(); ?>	
 
-					<p>Please B Seated We are a different kind of event rental company. We source hard-to-find items that. </span>
-					<?php the_content(); ?>		
-<br /><br /><br />
+					<?php endwhile; ?>		
+					
+					<br /><br /><br /><!--yikes.-->
 				
 				<div class="association one_half cf">
 					<div class="thumb">
@@ -29,9 +30,44 @@ get_template_part('header');?>
 							<p>Please B Seated We are a </p><br  />
 						</div>
 				</div><!--one_half-->
-				
+
+<?php wp_reset_query();?>
+		
+<?php
+$get_transit = new WP_Query( 'post_type=staff' );
+while ( $get_transit->have_posts() ) : $get_transit->the_post();
+$image =  get_post_meta($post->ID, 'info_image', true);
+$name =  get_post_meta($post->ID, 'info_name', true);
+$title =  get_post_meta($post->ID, 'info_title', true);
+$email =  get_post_meta($post->ID, 'info_email', true);
+$phone =  get_post_meta($post->ID, 'info_phone', true);
+?>
+
+
+	</div>
+	<?php endwhile;?>
+
 				<div class="one_half last">
- 
+
+
+
+					<div class="employee cf">
+						<div class="thumb">
+						<img src="http://placehold.it/140x100"/>
+						</div>
+						
+						<div class="content">
+							<span class="name"><?php echo $name;?></span><br  />						 
+							<span class="title"><?php echo $title;?></span><br  />
+							<a class="email" href="mailto:<?php echo $email;?>"><?php echo $email;?></a></span><br  />						  
+							<span class="phone"><?php echo $phone;?></span><br  />						 
+						</div>	
+					</div>
+
+				
+
+					<!--employee
+					
 					<div class="employee cf">
 						<div class="thumb">
 						<img src="http://placehold.it/140x100"/>
@@ -39,58 +75,20 @@ get_template_part('header');?>
 						
 						<div class="content">
 							<span class="name">Dave Jones</span><br  />						 
-							<span class="title">Principle</span><br  />	<br  />	
+							<span class="title">Principle</span><br  />
 							<a class="email" href="mailto:john@pleasebeseated.com">john@pleasebeseated.com</a></span><br  />						  
-							<span class="email">516.519.4823</span><br  />						 
+							<span class="phone">516.519.4823</span><br  />						 
 						</div>	
-					</div><!--employee-->
+					</div>
 
-
-								<div class="employee cf">
-						<div class="thumb">
-						<img src="http://placehold.it/140x100"/>
-						</div>
-						
-						<div class="content">
-							<span class="name">Dave Jones</span><br  />						 
-							<span class="title">Principle</span><br  />	<br  />	
-							<a class="email" href="mailto:john@pleasebeseated.com">john@pleasebeseated.com</a></span><br  />						  
-							<span class="email">516.519.4823</span><br  />						 
-						</div>	
-					</div><!--employee-->
-
-								<div class="employee cf">
-						<div class="thumb">
-						<img src="http://placehold.it/140x100"/>
-						</div>
-						
-						<div class="content">
-							<span class="name">Dave Jones</span><br  />						 
-							<span class="title">Principle</span><br  />	<br  />	
-							<a class="email" href="mailto:john@pleasebeseated.com">john@pleasebeseated.com</a></span><br  />						  
-							<span class="email">516.519.4823</span><br  />						 
-						</div>	
-					</div><!--employee-->
-
-								<div class="employee cf">
-						<div class="thumb">
-						<img src="http://placehold.it/140x100"/>
-						</div>
-						
-						<div class="content">
-							<span class="name">Dave Jones</span><br  />						 
-							<span class="title">Principle</span><br  />	<br  />	
-							<a class="email" href="mailto:john@pleasebeseated.com">john@pleasebeseated.com</a></span><br  />						  
-							<span class="email">516.519.4823</span><br  />						 
-						</div>	
-					</div><!--employee-->
+					employee-->
+ 
 
 				</div><!--one_half last-->
 
 		
 
 
-			<?php endwhile; ?>
 
 			 
 		</section><!--page body-->
