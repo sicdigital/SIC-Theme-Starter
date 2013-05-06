4e14f99e64488789535c0d5bdb1add21797e48ed
Michael Chacon <mike@sicdigital.com>
2013-05-06 12:09:00 -0400

    Removed loop from home page

--- template-home.php
+++ template-home.php
@@ -66,18 +66,7 @@ get_template_part('header');?>
 		<section id="primary_content">
 		
 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-				
-				<?php while ( have_posts() ) : the_post(); ?>	
-		
-				<div class="entry_content">											
 
-					<?php the_content(); ?>		
-
-				</div><!-- .entry-content -->
-
-			</article><!-- #post-<?php the_ID(); ?> -->
-
-			<?php endwhile; ?>
 
 		</section><!--primary_content-->
 			 
